fee88d596feb616f3130e2586c343fd74784a0e4
Ram Chandalada <ram_chandalada@mckinsey.com>
2022-09-13 10:03:31 +0530

    Sample flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h2> This is the first web app </h2>"
+    return "<h2> This my first web app by Ram</h2>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
