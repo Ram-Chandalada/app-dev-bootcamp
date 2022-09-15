7ae904f84aae9604f4f0e38852f8cb758ad273c7
Ram Chandalada <ram_chandalada@mckinsey.com>
2022-09-15 09:53:48 +0530

    Updated templates

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template,redirect
+
 
 app = Flask(__name__)
+app.config.update(
+    TESTING=True,
+    SECRET_KEY='ops_digital_asset'
+)
 
-@app.route("/")
-def hello_world():
-    return "<h2> This my first web app by Ram</h2>"
+@app.route('/',methods=['GET','POST'])
+def login():
+    user={"name":"Ram"}
+    return render_template("index.html",user=user)
 
+ 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
